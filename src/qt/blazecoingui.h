de87d56a9512730325ff99d4ceec740abd12b78e
Andrew Mason <194626045+AMA-CSharp-Code@users.noreply.github.com>
2026-05-12 22:26:29 +0100

    Revert "Qt GUI: pulsing/solid red status dot in the bottom-right of the wallet"
    This reverts commit 104569b64fef0403065247345d7ce58be215223c.

--- src/qt/blazecoingui.h
+++ src/qt/blazecoingui.h
@@ -106,12 +106,6 @@ private:
     QLabel *progressBarLabel;
     QProgressBar *progressBar;
 
-    // Status indicator (bottom-right of the wallet): pulses while the
-    // blockchain is syncing, solid red when fully caught up. Members so
-    // setNumBlocks() can start/stop the animation as the sync state moves.
-    class QGraphicsOpacityEffect *blazeIconOpacity;
-    class QPropertyAnimation     *blazeIconAnim;
-
     QMenuBar *appMenuBar;
     QAction *overviewAction;
     QAction *historyAction;
